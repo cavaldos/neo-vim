ce664c142f84eb74df41205f9e9ceb1924fd8c9d
cavaldos <khanhnguyen297kn@gmail.com>
2023-06-11 09:38:55 +0700

    init

---
+++ nvh/lua/user/lsp/init.lua
@@ -0,0 +1,20 @@
+-- Change diagnostic symbols in the sign column (gutter)
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+for type, icon in pairs(signs) do
+    local hl = "DiagnosticSign" .. type
+    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+
+vim.diagnostic.config({
+    virtual_text = true,
+    signs = true,
+    underline = true,
+    update_in_insert = false,
+    severity_sort = true,
+    float = {
+        border = "rounded",
+        source = "always"
+    }
+})
+
+require("user/lsp/servers")
